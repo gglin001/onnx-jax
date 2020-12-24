dccc12489ddcf5cb7d570625ee812f754d09ec61
gglin001 <guosonglin001@qq.com>
2020-12-25 00:07:19 +0800

    fix maxpool

--- onnx_jax/handlers/backend/maxpool.py
+++ onnx_jax/handlers/backend/maxpool.py
@@ -11,7 +11,8 @@ class MaxPool(BackendHandler):
     @classmethod
     def _common(cls, node, inputs, **kwargs):
         # TODO
-        pool_type = "MAX" if len(node.outputs) == 1 else "MAX_WITH_ARGMAX"
+        if len(node.outputs) > 1:
+            raise Exception('MaxPool with indices is not supported')
         return onnx_maxpool(inputs[0], **node.attrs)
 
     @classmethod
@@ -35,10 +36,33 @@ class MaxPool(BackendHandler):
         return cls._common(node, **kwargs)
 
 
-def onnx_maxpool(x, kernel_shape, pads=None, strides=None):
-    """Numpy-backed implementation of ONNX MaxPool op."""
-    prefix = (1,) * (x.ndim - len(kernel_shape))
-    dims = prefix + tuple(kernel_shape)
-    pads = tuple(pads) if pads else [0] * len(kernel_shape)
-    strides = (prefix + tuple(strides)) if strides else [1] * len(kernel_shape)
-    return [lax.reduce_window(x, -jnp.inf, lax.max, dims, strides, 'VALID')]
+def pad_helper(input_rank, pads=None):
+    pad_pairs = len(pads) // 2 if pads else 0
+
+    pad_width = []
+    for _ in range(input_rank - pad_pairs):
+        pad_width.append((0, 0))
+    for idx in range(pad_pairs):
+        pad_width.append((pads[idx], pads[idx + pad_pairs]))
+    return pad_width
+
+
+def onnx_maxpool(x, kernel_shape, pads=None, strides=None, dilations=None,
+                 auto_pad='NOTSET', ceil_mode=0, storage_order=0):
+
+    dims = (1,) * (x.ndim - len(kernel_shape)) + tuple(kernel_shape)
+    strides = ((1,) * (x.ndim - len(strides)) + tuple(strides)) if strides else (1,) * x.ndim
+    dilations = ((1,) * (x.ndim - len(dilations)) + tuple(dilations)) if dilations else (1,) * x.ndim
+
+    if auto_pad == "NOTSET":
+        pads = pad_helper(x.ndim, pads) if pads else 'VALID'
+    elif auto_pad == "SAME_UPPER":
+        pads = "SAME"
+    elif auto_pad == "VALID":
+        pads = "VALID"
+    elif auto_pad == "SAME_LOWER":
+        raise Exception("MaxPool with auto_pad `SAME_LOWER` Tensorflow")
+    else:
+        raise ValueError(f"Invalid auto_pad attribute: {auto_pad}")
+
+    return [lax.reduce_window(x, -jnp.inf, lax.max, dims, strides, pads, None, dilations)]
