b10e7a5b58e94a01e342020fad9dee01307b53f3
gglin001 <guosonglin001@qq.com>
2020-12-27 16:25:42 +0800

    add helper function for node test

--- tests/tools.py
+++ tests/tools.py
@@ -1,6 +1,8 @@
 import jax
 import jax.numpy as jnp
 
+from onnx_jax.backend import run_node
+
 random_key = jax.random.PRNGKey(0)
 
 
@@ -14,3 +16,14 @@ def _cosin_sim(a, b):
     b = b.flatten()
     cos_sim = jnp.dot(a, b) / (jnp.linalg.norm(a) * jnp.linalg.norm(b))
     return cos_sim
+
+
+def expect(node, inputs, outputs, **kwargs):
+    outputs_jax = run_node(node, inputs)
+
+    print(f"golden size: {outputs[0].shape}, output shape: {outputs_jax[0].shape}")
+    if list(outputs[0].shape) == list(outputs_jax[0].shape):
+        sim = _cosin_sim(outputs_jax[0], jnp.asarray(outputs[0]))
+        print(sim)
+    else:
+        print('failed')
