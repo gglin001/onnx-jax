7cb0b5ed2239832ee3e57ef8e3588810da2b3f03
gglin001 <guosonglin001@qq.com>
2021-04-20 23:53:01 +0800

    add slice_v1 test

--- tests/node/test_acosh.py
+++ tests/node/test_acosh.py
@@ -1,11 +1,7 @@
-import jax.numpy as jnp
 import numpy as np
 import onnx
-from onnx import defs, helper
-from onnx.helper import make_opsetid
 
-from onnx_jax.backend import run_node
-from tests.tools import cosin_sim, expect, gen_random
+from tests.tools import expect
 
 
 class Acosh:
